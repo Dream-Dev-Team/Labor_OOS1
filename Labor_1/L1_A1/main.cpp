bed5eece549b47bfe0099490a41875d8582653a8
Aaron Joel Müller <aamuit00@hs-esslingen.de>
2020-05-06 15:43:46 +0200

    hi

---
+++ Labor_1/L1_A1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//////////////////////////////////////////////////////
+//Dateiname: 
+//Autoren: Maxim Becht (mabeit10@hs-esslingen.de), Aaron Müller (aamuit00@hs-esslingen.de)
+//Enthaltene Module: 
+//Entwicklungsbeginn: xx.xx.xx		Entwicklungsende: xx.xx.xx
+//Zeitaufwand gesamt: 
+//Letzte Modifikationen: 
+//////////////////////////////////////////////////////
+
+int main()
+{
+
+}
