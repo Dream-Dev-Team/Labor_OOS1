e1d4b003daef67ef251e45f4ae7be07444c6a7f1
Maxim Becht <mabeit10@hs-esslingen.de>
2020-05-06 11:08:30 +0200

    Projektdateien hinzufügen.

---
+++ L1_A2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//////////////////////////////////////////////////////
+//Dateiname: 
+//Autoren: Maxim Becht (mabeit10@hs-esslingen.de), Aaron Müller (aamuit00@hs-esslingen.de)
+//Enthaltene Module: 
+//Entwicklungsbeginn: xx.xx.xx		Entwicklungsende: xx.xx.xx
+//Zeitaufwand gesamt: 
+//Letzte Modifikationen: 
+//////////////////////////////////////////////////////
+
+int main()
+{
+
+}
