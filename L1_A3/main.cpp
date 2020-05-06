dc2abf4b0907fe15a3fd7d3f41f0e672da61d2d2
Maxim Becht <mabeit10@hs-esslingen.de>
2020-05-06 12:56:10 +0200

    -für alle Laboraufgaben projekte hinzugefügt -für Labor1 Aufgaben: gegebene source codes hinzugefügt

--- L1_A3/main.cpp
+++ L1_A3/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <conio.h> // für _getch()
+// Achtung: _getch() ist nicht im Standard und 
+//          daher abhängig vom Compiler
 using namespace std;
 //////////////////////////////////////////////////////
 //Dateiname: 
@@ -9,7 +12,91 @@ using namespace std;
 //Letzte Modifikationen: 
 //////////////////////////////////////////////////////
 
-int main()
-{
 
+// Größe des Labyrinths
+const int kZeilen = 11;
+const int kSpalten = 11;
+
+// Zeichen, die im Labyrinth vorkommen können,
+// NL = new line, EOS = end of string
+enum Symbole {
+	MAUER = '#', WEG = ' ', MUENZE = ':', NL = '\n',
+	EOS = '\0', ICH = 'X', GEIST = 'G'
+};
+
+// Das Labyrinth als char-Array
+// Die vorletzte Spalte speichert ein Zeilenendezeichen \n
+// und die letzte ein \0-Zeichen, damit die Ausgabe leichter wird.
+char labyrinth[kZeilen][kSpalten + 2];
+
+// Labyrinth mit # füllen
+void initialisieren() {
+	/* HIER */
+
+
+
+
+
+
+
+
+
+}
+
+// Labyrinth auf dem Bildschirm ausgeben
+void drucken() {
+	// Console frei machen
+	system("cls");
+	// Labyrinth ausgeben
+	/* HIER */
+
+
+
+
+
+}
+
+// Hilfsfunktion max
+int max(int x, int y) {
+	return (x <= y) ? y : x;
+}
+
+// Hilfsfunktion min
+int min(int x, int y) {
+	return (x <= y) ? x : y;
+}
+
+// Durch Herumlaufen werden Wege im Labyrinth erzeugt
+// ASCII-Wert der Tasten: oben 72, links 75, rechts 77, unten 80
+void erzeugen() {
+	char c = 'x';
+	int posx = kSpalten / 2;
+	int posy = kZeilen / 2;
+	labyrinth[posy][posx] = ICH;
+	drucken();
+	while (c != 'q') {
+		drucken();
+		cout << "Laufen mit Pfeiltasten. Beenden mit q." << endl;
+		labyrinth[posy][posx] = WEG;
+		c = _getch();
+		switch (int(c)) {
+			// oben
+		case 72: posy = max(1, posy - 1); break;
+			// links
+		case 75: posx = max(1, posx - 1); break;
+			// rechts
+		case 77: posx = min(kSpalten - 2, posx + 1); break;
+			// unten
+		case 80: posy = min(kZeilen - 2, posy + 1); break;
+			// q = quit
+		case 113: break;
+		}
+		labyrinth[posy][posx] = ICH;
+	}
+}
+
+int main() {
+	initialisieren();
+	drucken();
+	erzeugen();
 }
--- L1_A4/main.cpp
+++ L1_A4/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 using namespace std;
 //////////////////////////////////////////////////////
 //Dateiname: 
@@ -9,7 +12,136 @@ using namespace std;
 //Letzte Modifikationen: 
 //////////////////////////////////////////////////////
 
-int main()
+/*
+
+Spiel Tsching, Tschang, Tschong
+
+Regeln:
+Es gibt zwei Spieler: der Programmierer und der Computer.
+Beide Spieler waehlen gleichzeitig eines der drei Objekte
+Stein, Schere und Papier. Beide können das gleiche Objekt wählen.
+Der Spieler, der das mächtigere Objekt gewählt hat gewinnt.
+SCHERE kann einen STEIN nicht zerschneiden, d.h. STEIN ist
+mächtiger als SCHERE. PAPIER wickelt STEIN ein, d.h. PAPIER ist
+mächtiger als STEIN. SCHERE zerschneidet PAPIER, d. h. SCHERE
+ist maechtiger als PAPIER
+
+*/
+
+
+// Aufzählungstyp für Stein etc.
+enum objectType { STEIN, SCHERE, PAPIER };
+
+// Struktur für einen Spieler bestehend aus Name und Wahl 
+// des Spielers
+struct player {
+	char* name;
+	objectType choice;
+};
+
+// Variable für den Namen des Spielers 
+char name[15];
+
+// Name des Spielers eingeben
+char* enterName(char str[])
 {
 
+
+
+
+
+}
+
+// Den Computer zufällig waehlen lassen. 
+// Nutzen Sie srand(...) und rand().
+objectType randomChoice()
+{
+
+
+
+
+}
+
+// Die Wahl von STEIN etc. als String zurückgeben lassen
+char* object2str(objectType o)
+{
+
+
+
+
+
+
+
+
+
+}
+
+// Einen Text mit dem Namen des Spielers und seiner Wahl ausgeben
+void showPlayer(player p)
+{
+
+
+
+}
+
+// Die Wahl des Spielers abfragen
+objectType enterChoice()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+// Die Wahl bestimmen, die gewonnen hat
+objectType winningObject(objectType obj1, objectType obj2)
+{
+
+
+
+
+
+
+}
+
+// Ausgeben, wer gewonnen hat
+void showWinner(player p1, player p2)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+int main()
+{
+	player player1, player2;
+	player1.name = "Computer";
+	player1.choice = randomChoice();
+	cout << "Der Computer hat sein Wahl getroffen." << endl;
+	player2.name = enterName(name);
+	player2.choice = enterChoice();
+	showPlayer(player1);
+	showPlayer(player2);
+	showWinner(player1, player2);
 }
