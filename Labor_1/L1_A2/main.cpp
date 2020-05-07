6415e1a89737c5cf435d2c5921197ef2b49ae10e
Aaron Joel Müller <aamuit00@hs-esslingen.de>
2020-05-07 10:29:01 +0200

    Aufgabe 2 fertig

--- Labor_1/L1_A2/main.cpp
+++ Labor_1/L1_A2/main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 using namespace std;
 //////////////////////////////////////////////////////
-//Dateiname: 
+//Dateiname: Labor_1_Aufgabe_2
 //Autoren: Maxim Becht (mabeit10@hs-esslingen.de), Aaron Müller (aamuit00@hs-esslingen.de)
-//Enthaltene Module: 
-//Entwicklungsbeginn: xx.xx.xx		Entwicklungsende: xx.xx.xx
-//Zeitaufwand gesamt: 
+//Enthaltene Module: -
+//Entwicklungsbeginn: 07.05.20		Entwicklungsende: 07.05.20
+//Zeitaufwand gesamt: 35  min
 //Letzte Modifikationen: 
 //////////////////////////////////////////////////////
 
+void eingabe(int[], int);
+void ausgabe(int[], int);
 int main()
 {
+	int length;
+	cout << "Wie viele Zahlen m\x94 \bchten Sie einlesen?" << endl;
+	cin >> length;
+	int * array = new int[length];
+	if (array == NULL){
+		cerr << "Fehler beim Erstellen eines Arrays";
+		return 1;
+	}
+	eingabe(array, length);
+	ausgabe(array, length);
+}
 
+void eingabe(int array[], int length) {
+	for (int i = 0; i < length; ++i) {
+		cout << "Geben Sie das " << i + 1 << ". Element ein: ";
+		cin >> array[i];
+	}
+}
+
+void ausgabe(int array[] , int length) {
+	float avg = 0;
+	for (int i = 0; i < length; ++i) {
+		avg += array[i];
+		cout << i + 1 << ". Element des Arrays: " << array[i] << endl;
+	}
+	avg /= length;
+	cout << "Durchschnitt aller Elemente des Arrays: " << avg;
 }
